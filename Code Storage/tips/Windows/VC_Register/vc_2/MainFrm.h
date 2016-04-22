ccfa2cf553733950aac9522ab0675b428a8be0c3
彭水明 <lilingshui@126.com>
2016-04-22 17:30:25 +0800

    add vc register project

---
+++ Code Storage/tips/Windows/VC_Register/vc_2/MainFrm.h
@@ -0,0 +1,43 @@
+// MainFrm.h : CMainFrame 类的接口
+//
+
+#pragma once
+#include "ChildView.h"
+
+class CMainFrame : public CFrameWnd
+{
+
+public:
+	CMainFrame();
+protected: 
+	DECLARE_DYNAMIC(CMainFrame)
+
+	// 重写
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	virtual BOOL OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo);
+
+	// 实现
+public:
+	virtual ~CMainFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+	// 生成的消息映射函数
+protected:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnSetFocus(CWnd *pOldWnd);
+	DECLARE_MESSAGE_MAP()
+
+public:
+	afx_msg void OnPaint();
+	afx_msg void OnGood();
+
+
+protected:  // 控件条嵌入成员
+	CToolBar          m_wndToolBar;
+	CStatusBar        m_wndStatusBar;
+	CChildView		  m_wndView;
+};
